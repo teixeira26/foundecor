b20a1703c794fb7c796bd69130090f1e892e613a
Mathzilla <teixeira.matheus26@gmail.com>
2023-11-15 15:17:56 -0300

    feat - add landing

--- organisms/initial.tsx
+++ organisms/initial.tsx
@@ -13,16 +13,16 @@ export default function Initial({}: Props) {
       </h2>
       <aside className="flex gap-4">
         <div className="flex flex-col gap-2 justify-baseline font-fanwood ">
-            <p className="text-[#16a34a] text-[32px] text-left max-w-[80px]">18+</p>
-            <p className="text-white text-[16px] text-left max-w-[80px]">Años en el mercado</p>
+            <p className="text-[#16a34a] text-[32px] max-w-[140px] text-center">18+</p>
+            <p className="text-white text-[16px] max-w-[140px] text-center">Años en el mercado</p>
         </div>
         <div className="flex flex-col gap-2 justify-baseline font-fanwood ">
-            <p className="text-[#16a34a] text-[32px] text-left max-w-[100px]">1000+</p>
-            <p className="text-white text-[16px] text-left max-w-[100px]">Ventas en Mercado Libre</p>
+            <p className="text-[#16a34a] text-[32px] max-w-[140px] text-center">1000+</p>
+            <p className="text-white text-[16px] max-w-[140px] text-center">Ventas en Mercado Libre</p>
         </div>
-        <div className="flex flex-col gap-1 justify-baseline font-fanwood ">
-            <p className="text-[#16a34a] text-[32px] text-left max-w-[80px]">200 +</p>
-            <p className="text-white text-[16px] text-left max-w-[80px]">Evaluaciones positivas</p>
+        <div className="flex flex-col gap-1 justify-baseline font-fanwood text-center ">
+            <p className="text-[#16a34a] text-[32px] max-w-[140px] text-center">200 +</p>
+            <p className="text-white text-[16px] max-w-[140px] text-center">Evaluaciones positivas</p>
         </div>
       </aside>
       <button className="px-4 py-2 bg-[#16a34a] font-hind rounded-[5px]">
