703c79cec70556c7dc3ab19c9bd94deb06c77bcd
teixeira26 <teixeira.matheus26@gmail.com>
2023-11-27 14:31:28 -0300

    feat: add google verification tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Foundecor',
   description: 'Descubre la mejor selección de pasta americana de calidad, con diferentes colores, sabores y texturas. Descubre cómo convertir tus pasteles en verdaderas obras de arte que dejarán a todos con la boca abierta. ¡Te esperamos!',
+  verification:{
+    google: '4o79rGt3k5im2kaRas8MNjT2w3v65mbNGC9fUhxTfys'
+  }
 }
 
 export default function RootLayout({
